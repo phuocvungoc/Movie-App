2f54938fbb5606b86d7f6fd26e25c6a4af47aa78
Vu Ngoc Phuoc <phuocvungoc9997@gmail.com>
2022-09-20 13:13:07 +0700

    Lấy phim theo rating

--- BACKEND/controllers/movie.js
+++ BACKEND/controllers/movie.js
@@ -11,8 +11,21 @@ exports.getMovieTrending = (req, res, next) => {
   movieList.fetchAll((movieList) => {
     const movieListTrending = movieList;
     movieListTrending.sort((a, b) => b.popularity - a.popularity);
-    res.send({
+    res.statusCode(200).send({
       results: movieListTrending.slice(page * 20 - 20, page * 20),
+      page: page,
+      total_pages: movieList.length / 20,
+    });
+  });
+};
+
+exports.getMovieTopRate = (req, res, next) => {
+  const page = req.params.page;
+  movieList.fetchAll((movieList) => {
+    const movieListTopRate = movieList;
+    movieListTopRate.sort((a, b) => b.vote_average - a.vote_average);
+    res.statusCode(200).send({
+      results: movieListTopRate.slice(page * 20 - 20, page * 20),
       page: page,
       total_pages: movieList.length / 20,
     });
--- BACKEND/routes/movie.js
+++ BACKEND/routes/movie.js
@@ -9,4 +9,6 @@ router.get("/movies", movieListController.getMovieList);
 
 router.get("/movies/trending/:page", movieListController.getMovieTrending);
 
+router.get("/movies/top-rate/:page", movieListController.getMovieTopRate);
+
 module.exports = router;
