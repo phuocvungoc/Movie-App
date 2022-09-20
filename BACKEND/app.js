93199ec825ec15fa212d26a24618aeb33f423283
Vu Ngoc Phuoc <phuocvungoc9997@gmail.com>
2022-09-20 08:57:23 +0700

    Movie-App

---
+++ BACKEND/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(express.json()); // for parsing application/json
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.listen(5000);
