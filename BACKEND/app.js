e28317e8a895ede0ac52e4dbf144b595a1766ea4
Vu Ngoc Phuoc <phuocvungoc9997@gmail.com>
2022-09-22 16:10:53 +0700

    Thêm trang 404 và middleware xác thực token

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -3,12 +3,19 @@ const cors = require("cors");
 const app = express();
 
 const movieRoutes = require("./routes/movie");
+const authorizationMiddleWare = require("./middleware/Authorization");
 
 app.use(express.json()); // for parsing application/json
 app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", movieRoutes);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found on the server</h1>");
+});
+
+app.post("/login", authorizationMiddleWare.authorization);
+
+app.use("/api", authorizationMiddleWare.authToken, movieRoutes);
 
 app.listen(5000);
